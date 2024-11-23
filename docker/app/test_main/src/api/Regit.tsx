0b2b30b9af8ba32b4080af7861e0df4472507c3f
unknown <keita.goto>
2024-11-23 21:55:16 +0900

    [追加]SPA設定の追加

---
+++ docker/app/test_main/src/api/Regit.tsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../assets/css/Main.css';
+
+
+function Redit() {
+
+  return (
+    <div className="container">
+        <p>これはてすと</p>
+    </div>
+  );
+}
+
+export default Redit;
