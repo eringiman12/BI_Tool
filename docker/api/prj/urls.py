9f633e87ca96e98777bf98e6e25ea690a7ec0dfd
unknown <keita.goto>
2024-11-25 21:34:13 +0900

    [追加]更新処理の追加

--- docker/api/prj/urls.py
+++ docker/api/prj/urls.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from main import views
+from main.views import EsteTestViewSet
 
 router = DefaultRouter()
-router.register(r'este', views.EsteViewSet, basename='este')
-router.register(r'este-regist', views.EsteRegitSet, basename='este-register')
+router.register(r'este', EsteTestViewSet, basename='este')
 
 urlpatterns = [
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path('api/este-list/', EsteTestViewSet.as_view(), name='este-list'),
+    path('api/este-regist/', EsteTestViewSet.as_view(), name='este-register'),
+    path('api/este-update/<int:pk>/', EsteTestViewSet.as_view(), name='este-update')
+    # path('este-update/<int:pk>/', views.EsteUpdateView.as_view(), name='este-update'),
 ]
